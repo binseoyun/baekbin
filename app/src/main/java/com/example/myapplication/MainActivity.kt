7357f5536e7d7a8ffd87ff0a5c6996812efcf60c
jaeho <jeeno816@kaist.ac.kr>
2025-07-03 16:45:22 +0900

    백재호가 바꿈

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "binseoyun",
+                        name = "baekjaeho",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
