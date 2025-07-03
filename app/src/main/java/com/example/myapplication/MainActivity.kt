6ab9bf1625a9ba702ec5a40ef194447fee28e204
binseoyun <sunnybin04@gmail.com>
2025-07-03 16:43:14 +0900

    hello world

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "World",
+                        name = "binseoyun",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
